b4e66ab65f6ab6823487d2a8cc1e7e1daab638e9
barinder <barindersingh1997@gmail.com>
2025-04-10 16:47:02 +0530

    rolldice images shifted to assts

--- src/components/Rolldice.jsx
+++ src/components/Rolldice.jsx
@@ -1,6 +1,22 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 
+import dice1 from '../assets/dice/dice_1.png';
+import dice2 from '../assets/dice/dice_2.png';
+import dice3 from '../assets/dice/dice_3.png';
+import dice4 from '../assets/dice/dice_4.png';
+import dice5 from '../assets/dice/dice_5.png';
+import dice6 from '../assets/dice/dice_6.png';
+
+const diceImages = {
+    1: dice1,
+    2: dice2,
+    3: dice3,
+    4: dice4,
+    5: dice5,
+    6: dice6,
+  };
+
 const Rolldice = ({roleDice,currentdice}) => {
    
 
@@ -10,7 +26,11 @@ const Rolldice = ({roleDice,currentdice}) => {
   return (
     <Dicecontainer>
       <div className='dice' onClick={roleDice}>
-        <img src={`/images/dice/dice_${currentdice}.png`}/>
+        {/* <img src={`/images/dice/dice_${currentdice}.png`}/> */}
+        {/* <img src={`dice${currentdice}`}/> */}
+        <img src={diceImages[currentdice]} alt={`Dice ${currentdice}`} />
+
+        {/* <img src={dice{currentdice}}/> */}
       </div>
       <p>Click on dice to roll</p>
     </Dicecontainer>
